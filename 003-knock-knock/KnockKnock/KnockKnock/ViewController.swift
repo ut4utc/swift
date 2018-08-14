9f73700437f9ba68f5a729b703d367e07e83cdab
= <=>
2018-08-14 11:59:27 +0545

    update project

--- 003-knock-knock/KnockKnock/KnockKnock/ViewController.swift
+++ 003-knock-knock/KnockKnock/KnockKnock/ViewController.swift
@@ -14,7 +14,18 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func KnockKnock () {
+        
+        let alert = UIAlertController(title: "Knock Knock", message: "Hi I'm Knock Knock", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Cool", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
 
 }
 
